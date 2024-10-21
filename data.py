43924ed3435c29b0657671e6a0d6a5f953a4ddb5
Emirhan Ülgen <98956130+emugre42@users.noreply.github.com>
2024-10-21 20:58:17 +0100

    Create data.ipynb to data.py

--- data.py
+++ data.py
@@ -1,7 +1,6 @@
 data = [5, "Python", 3.14, -2, "Data", True, 0, 12.5, 9, "AI", None, 4]
 integer = filter(lambda x: type(x) == int, data)
 integer = list(integer)
-integer
 
 squares = map(lambda x: x**2, integer)
 squares = list(squares)
