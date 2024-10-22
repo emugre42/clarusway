66c929f12fbfddbbe056e272fa66efff1991f752
Emirhan Ülgen <98956130+emugre42@users.noreply.github.com>
2024-10-22 01:43:34 +0100

    Task 7

--- task7.py
+++ task7.py
@@ -2,4 +2,4 @@ age = int(input("Enter your age: "))
 
 output = "Welcome, and Have fun!" if age >= 18 else "Please scan your parent's ID first" if age < 18 and age >= 16 else "Return to your home!"
 
-output
+print(output)
